bf2223e0c114b0dba5b10d96e8924130058071cc
Ashvin Santhosh <vrindtime@gmail.com>
2024-08-19 05:12:04 +0530

    minor fix , added navigatino to all pages

--- navbar.js
+++ navbar.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const path = window.location.pathname.split('/').pop(); // Get the current file name
 
     // If the file name is empty, it means we're on the home page (index.html)
-    const isHomePage = path === '' || path === 'index.html';
+    const isHomePage = path === './' || path === './index.html';
 
     // Get all navigation links
     const navLinks = document.querySelectorAll('nav a');
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Set the active class based on the current page URL
     navLinks.forEach(link => {
         const href = link.getAttribute('href').split('/').pop(); // Get the link's file name
-        if (isHomePage && href === 'home.html' || path === href) {
+        if (isHomePage && href === './index.html' || path === href || href === '') {
             link.classList.add('active');
         } else {
             link.classList.remove('active');
